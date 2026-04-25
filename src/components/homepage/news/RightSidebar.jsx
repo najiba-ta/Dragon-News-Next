3a81973408d68ac5572d243ab6afb7deca975410
najiba-ta <shahidnajiba@gmail.com>
2026-04-25 06:17:17 +0600

    fix righsidebar

--- src/components/homepage/news/RightSidebar.jsx
+++ src/components/homepage/news/RightSidebar.jsx
@@ -1,16 +1,16 @@
-// import { FaGoogle } from "react-icons/fa";
-// import { FaGithub } from "react-icons/fa";
-// const RightSidebar = () => {
-//     return (
-//         <div>
-//             <h2 className="font-bold text-lg mb-4">Login with</h2>
-//           <div className="flex flex-col gap-4">
-//               <button className="btn btn-dash text-yellow-500"><FaGoogle /> Login with google</button>
-//             <button className="btn btn-dash text-pink-600"><FaGithub /> Login with github</button>
-//           </div>
-//         </div>
-//     );
-// };
+import { FaGoogle } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+const RightSidebar = () => {
+    return (
+        <div>
+            <h2 className="font-bold text-lg mb-4">Login with</h2>
+          <div className="flex flex-col gap-4">
+              <button className="btn btn-dash text-yellow-500"><FaGoogle /> Login with google</button>
+            <button className="btn btn-dash text-pink-600"><FaGithub /> Login with github</button>
+          </div>
+        </div>
+    );
+};
 
 
-// export default RightSidebar;
+export default RightSidebar;
