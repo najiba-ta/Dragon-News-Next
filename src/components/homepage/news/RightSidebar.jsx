6eadcae415ee437de7019c3436d017285452f064
najiba-ta <shahidnajiba@gmail.com>
2026-04-28 05:35:05 +0600

    added something

--- src/components/homepage/news/RightSidebar.jsx
+++ src/components/homepage/news/RightSidebar.jsx
@@ -21,7 +21,7 @@ const RightSidebar =() => {
     <div>
       <h2 className="font-bold text-lg mb-4">Login with</h2>
       <div className="flex flex-col gap-4">
-        <button className="btn btn-dash text-yellow-500" onClick={handleGoogleSignin}><FaGoogle /> Login with google</button>
+        <button className="btn btn-dash text-yellow-500" onClick={handleGoogleSignin}><FaGoogle /> Loginwith google</button>
         <button className="btn btn-dash text-pink-600" onClick={handleGithubSignin}><FaGithub /> Login with github</button>
       </div>
     </div>
